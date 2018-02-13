ea510300dfc17d888691a5ff9d286bbededd4b6d
Avaer Kazmer <a@modules.io>
2018-02-12 23:37:37 -0500

    Transpite to version 150 instead of 300 es

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var version = require('glsl-version-regex')
 var inject = require('glsl-token-inject-block')
+var tokenize = require('glsl-tokenizer')
+var stringify = require('glsl-token-string')
 
-module.exports.vertex = transpile100to300.bind(null, true)
-module.exports.fragment = transpile100to300.bind(null, false)
+module.exports.vertex = transpile.bind(null, true)
+module.exports.fragment = transpile.bind(null, false)
 
 var coreGLSLExtensions = [
   'GL_OES_standard_derivatives',
@@ -13,65 +15,63 @@ var coreGLSLExtensions = [
 
 var reservedWords = require('./lib/builtins')
 
-function transpile100to300 (isVertex, tokens) {
+function transpile(isVertex, source) {
+  var tokens = tokenize(source)
   var oldVersion = versionify(tokens)
-  if (oldVersion === '300 es') {
-    // already in version 300, seems OK
-    return tokens
-  }
+  if (oldVersion !== '150') {
+    var nameCache = {}
+    var reservedNameCache = {}
+    var fragColorName = null
+    var fragDepthName = null
+    var i, token
+    for (i = 0; i < tokens.length; i++) {
+      token = tokens[i]
+      if (token.type === 'keyword') {
+        if (token.data === 'attribute') token.data = 'in'
+        else if (token.data === 'varying') token.data = isVertex ? 'out' : 'in'
+      } else if ((token.type === 'builtin') && /^texture(2D|Cube)?/.test(token.data)) {
+        token.data = token.data.replace(/(2D|Cube|EXT)/g, '')
+      } else if (token.type === 'builtin' && !isVertex) {
+        if (token.data === 'gl_FragColor') {
+          if (!fragColorName) {
+            fragColorName = getUniqueName(tokens, nameCache, 'fragColor')
+            insertFragOutput(tokens, fragColorName, 'vec4')
+          }
+          token.data = fragColorName
+        } else if (token.data === 'gl_FragDepth') {
+          if (!fragDepthName) {
+            fragDepthName = getUniqueName(tokens, nameCache, 'fragDepth')
+            insertFragOutput(tokens, fragDepthName, 'float')
+          }
+          token.data = fragDepthName
+        }
+      } else if (token.type === 'ident' && reservedWords.indexOf(token.data) >= 0) {
+        if (isVertex && isAttribtue(tokens, i)) {
+          throw new Error('Unable to transpile GLSL 100 to 150 automatically:\n' +
+              'One of the vertex shader attributes is using a reserved 150 keyword "' + token.data + '"')
+        }
+        if (!(token.data in reservedNameCache)) {
+          reservedNameCache[token.data] = getUniqueName(tokens, nameCache, token.data)
+        }
+        token.data = reservedNameCache[token.data]
+      }
+    }
 
-  var nameCache = {}
-  var reservedNameCache = {}
-  var fragColorName = null
-  var fragDepthName = null
-  var i, token
-  for (i = 0; i < tokens.length; i++) {
-    token = tokens[i]
-    if (token.type === 'keyword') {
-      if (token.data === 'attribute') token.data = 'in'
-      else if (token.data === 'varying') token.data = isVertex ? 'out' : 'in'
-    } else if ((token.type === 'builtin') && /^texture(2D|Cube)?/.test(token.data)) {
-      token.data = token.data.replace(/(2D|Cube|EXT)/g, '')
-    } else if (token.type === 'builtin' && !isVertex) {
-      if (token.data === 'gl_FragColor') {
-        if (!fragColorName) {
-          fragColorName = getUniqueName(tokens, nameCache, 'fragColor')
-          insertFragOutput(tokens, fragColorName, 'vec4')
+    for (i = tokens.length - 1; i >= 0; i--) {
+      token = tokens[i]
+      if (token.type === 'preprocessor') {
+        var match = token.data.match(/\#extension\s+(.*)\:/)
+        if (match && match[1] && coreGLSLExtensions.indexOf(match[1].trim()) >= 0) {
+          var nextToken = tokens[i + 1]
+          var count = (nextToken && nextToken.type === 'whitespace')
+            ? 2 : 1
+          tokens.splice(i, count)
         }
-        token.data = fragColorName
-      } else if (token.data === 'gl_FragDepth') {
-        if (!fragDepthName) {
-          fragDepthName = getUniqueName(tokens, nameCache, 'fragDepth')
-          insertFragOutput(tokens, fragDepthName, 'float')
-        }
-        token.data = fragDepthName
       }
-    } else if (token.type === 'ident' && reservedWords.indexOf(token.data) >= 0) {
-      if (isVertex && isAttribtue(tokens, i)) {
-        throw new Error('Unable to transpile GLSL 100 to 300 automatically:\n' +
-            'One of the vertex shader attributes is using a reserved 300es keyword "' + token.data + '"')
-      }
-      if (!(token.data in reservedNameCache)) {
-        reservedNameCache[token.data] = getUniqueName(tokens, nameCache, token.data)
-      }
-      token.data = reservedNameCache[token.data]
     }
   }
 
-  for (i = tokens.length - 1; i >= 0; i--) {
-    token = tokens[i]
-    if (token.type === 'preprocessor') {
-      var match = token.data.match(/\#extension\s+(.*)\:/)
-      if (match && match[1] && coreGLSLExtensions.indexOf(match[1].trim()) >= 0) {
-        var nextToken = tokens[i + 1]
-        var count = (nextToken && nextToken.type === 'whitespace')
-          ? 2 : 1
-        tokens.splice(i, count)
-      }
-    }
-  }
-
-  return tokens
+  return stringify(tokens)
 }
 
 function isAttribtue (tokens, index) {
@@ -124,19 +124,19 @@ function insertFragOutput (tokens, name, dataType) {
   ])
 }
 
-function versionify (tokens) {
+function versionify(tokens) {
   for (var i = 0; i < tokens.length; i++) {
     var token = tokens[i]
     if (token.type === 'preprocessor') {
       var match = version.exec(token.data)
       if (match) {
         var number = match[1].replace(/\s\s+/g, ' ')
-        if (number === '300 es') {
-          // this shader is already in 300 es
+        if (number === '150') {
+          // this shader is already in 150
           return number
         } else if (number === '100') {
           tokens.splice(i, 1, {
-            data: '#version 300 es',
+            data: '#version 150',
             type: 'preprocessor'
           })
           return number
@@ -149,7 +149,7 @@ function versionify (tokens) {
 
   // no version found, insert into start
   tokens.splice(0, 0, {
-    data: '#version 300 es',
+    data: '#version 150',
     type: 'preprocessor'
   }, {
     data: '\n',
